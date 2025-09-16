38ac5c838aae89e2fe4fd8b2b359aaa8af6161b8
Ty Schaub <tyschaub@Tys-MacBook-Air.local>
2025-09-16 15:22:12 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        double purchasePrice = 100;
+        final double SALES_TAX_RATE = 0.05; // 5 % sales tax
+
+        double salesTax = purchasePrice * SALES_TAX_RATE;
+        double totalPrice = purchasePrice + salesTax;
+
+        System.out.println("Purchase Price : $ " + purchasePrice);
+        System.out.println("Sales Tax : $ " + salesTax);
+        System.out.println("Total Price : $ " + totalPrice);
+    }
+}
